b4f6acb0fd6ea4c871fcc37b7c32a015d6ea68e5
BarnaliD <115993238+BarnaliD@users.noreply.github.com>
2024-11-06 12:37:18 +0100

    corrected fail test

--- src/test/java/org/example/RectangleTest.java
+++ src/test/java/org/example/RectangleTest.java
@@ -43,6 +43,6 @@ class RectangleTest {
     @Test
     void testCalculateAreaWithIncorrectExpectedValue() {
         Rectangle rectangle = new Rectangle(5, 10);
-        assertEquals(45, rectangle.calculateArea(), "This test is designed to fail. Expected area is intentionally incorrect.");
+        assertEquals(50, rectangle.calculateArea(), "This test is designed to fail. Expected area is intentionally incorrect.");
     }
 }
